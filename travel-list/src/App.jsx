d9df0c416a9697a9838c618784fcd42efbbb893b
JavaCoderSubham <124339547+JavaCoderSubham@users.noreply.github.com>
2024-05-29 13:04:12 +0530

    App basic Structure done

---
+++ travel-list/src/App.jsx
@@ -0,0 +1,36 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        👜 You have X items on Your list, and you have already packed X (X%)
+      </em>
+    </footer>
+  );
+}
